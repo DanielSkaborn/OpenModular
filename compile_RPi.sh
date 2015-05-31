7bc0816f7af45004934d6d3fc72b8cf3b7488a5e
Daniel Skaborn <d_skaborn@yahoo.com>
2015-05-31 19:03:06 +0000

    corrected bus init when no patchfiles. and set audio to RPi internal.

--- compile_RPi.sh
+++ compile_RPi.sh
@@ -1,1 +1,1 @@
-gcc OpenModular.c -O2 -mcpu=arm1176jzf-s -mfpu=vfp -mfloat-abi=hard -lasound -pthread -o OpenModular -D TEXTEDIT -D ALSA_RPI
+gcc OpenModular.c -O2 -mcpu=arm1176jzf-s -mfpu=vfp -mfloat-abi=hard -lasound -lm -pthread -o OpenModular -D TEXTEDIT -D ALSA_RPI
